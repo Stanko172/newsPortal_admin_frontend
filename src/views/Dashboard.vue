f8e9740e58b7332d03cade458a43e8d3918fa6ad
Stanko172 <stanko.bebek00@gmail.com>
2021-07-31 14:16:22 +0200

    testing image upload

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,6 +8,23 @@
       </div>
 
       <button @click.prevent="handleLogout">Logout</button>
+
+      <h1>Image upload testing!</h1>
+      <div>
+          Učitaj sliku:
+          <input type="file" v-on:change="onChange"> <br><br>
+
+          <input type="button" value="Učitaj" @click="formSubmit"> <br /><br />
+
+          <img v-bind:src="imagePreview" width="100" height="100" v-show="showPreview"/> 
+
+            
+
+          <div v-if="success != ''">
+            {{success}}
+            <img src="http://127.0.0.1:8000/storage/uploads/1627732403_mm.png" alt="">
+          </div>
+      </div>
   </div>
 </template>
 
@@ -21,7 +38,11 @@ export default {
     },
     data(){
         return{
-
+            name: '',
+            file: '',
+            success: '',
+            imagePreview: null,
+            showPreview: false,
         }
     },
     computed:{
@@ -31,7 +52,53 @@ export default {
         ...mapActions('auth', ['logout', 'getUser']),
         handleLogout(){
             this.logout()
-        }
+        },
+        onChange(e) {
+                this.file = e.target.files[0];
+
+                let reader  = new FileReader();
+
+                reader.addEventListener("load", function () {
+                    this.showPreview = true;
+                    this.imagePreview = reader.result;
+                }.bind(this), false);
+
+                 if( this.file ){
+                    /*
+                        Ensure the file is an image file.
+                    */
+                    if ( /\.(jpe?g|png|gif)$/i.test( this.file.name ) ) {
+
+                        console.log("here");
+                        /*
+                        Fire the readAsDataURL method which will read the file in and
+                        upon completion fire a 'load' event which we will listen to and
+                        display the image in the preview.
+                        */
+                        reader.readAsDataURL( this.file );
+                    }
+                }
+            },
+        formSubmit(e) {
+            e.preventDefault();
+            let existingObj = this;
+
+            let data = new FormData();
+            data.append('file', this.file);
+            data.append('title', 'Testni upload');
+            data.append('body', 'Testni upload tekst.');
+            data.append('category_id', 1);
+
+            console.log(data)
+
+            api.post('/auth/articles/create', data)
+            .then(function (res) {
+                existingObj.success = res.data.success;
+            })
+            .catch(function (err) {
+                existingObj.output = err;
+            });
+            }
     },
     mounted(){
 
